53f08a8da47e163fe163449cd0a3cfbe02a3903f
Akpevwe <essiakpevwepraise@gmail.com>
2024-06-21 18:57:54 +0100

    stripe api

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
 <?php
 
-$ch = curl_init();
+$api_key = "YOUR_API_KEY";
 
-$headers = [
-    'Authorization: token YOUR_TOKEN',
-    'Content-Type: application/json'
-    ];
+$data = [
+    "name" => "Alice",
+    "email" => "alice@example.com"
+];
 
-$payload = json_encode([
-    'name' => 'Created from API',
-    'description' => 'an example-API created example',
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+    CURLOPT_URL => "https://api.strie.com/v1/customers",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_USERPWD => $api_key . ":",
+    CURLOPT_POSTFIELDS => $data,
 ]);
 
-curl_setopt_array($ch, [
-    CURLOPT_URL => "https://api.github.com/user/repos",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => $headers,
-    CURLOPT_USERAGENT => 'cURL',
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POST => true,
-    CURLOPT_POSTFIELDS => $payload
+$response = curl_exec($curl);
 
-    ]);
+curl_close($curl);
 
-$response = curl_exec($ch);
-
-$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
